0a5b4431c1d32a5538937e4f009e59ca6ae82b9e
Victoria Tran <victoriatran@Victorias-MacBook-Air.local>
2025-12-02 13:03:27 +0100

    updating meta info

--- src/lib/data/meta.ts
+++ src/lib/data/meta.ts
@@ -2,20 +2,21 @@
 // So they can be added as suffixes on different pages
 // Via <svelte:head>
 
-export const siteBaseUrl = 'https://sveltekit-static-blog-template.vercel.app/';
+export const siteBaseUrl = 'https://victorialabmayr.com/';
 
 export const keywords = [
-	'Svelte',
-	'SvelteKit',
-	'Template',
-	'Blog',
-	'Starter',
-	'Static Site'
+  'Victoria Labmayr',
+  'Data Scientist',
+  'Svelte Developer',
+  'Geo Visualization',
+  'Machine Learning',
+  'Creative Tech'
 ];
 
+
 export const description =
-	"A light, neat, and easy-to-use SvelteKit template for your next website.";
+	"Victoria (a crafty little knitter). Victoria (a clever data thinker). Victoria (an artsy little maker).";
 
 export const title = 'My lil website';
 
-export const image = `${siteBaseUrl}/images/site-preview.png`;
+export const image = `${siteBaseUrl}/images/miffy.png`;
